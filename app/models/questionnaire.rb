26fe8cf1aebecc73d8eb329d9bcb54774bc3cdb7
jai kirdatt <jai.kirdatt@me.com>
2019-01-09 23:17:21 -0500

    selected questions randomly

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -3,6 +3,7 @@ class Questionnaire < ApplicationRecord
 
   def self.top_unasked_question_for_category(category_id, existing_questions)
     Questionnaire.limit(1)
+                 .order("RANDOM()")
                  .select("title", "answer")
                  .where(category_id: category_id)
                  .where.not(title: existing_questions).pluck(:title, :answer)
