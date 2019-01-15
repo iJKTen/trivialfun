2f61716c79cd296a185b2ced1c6538bc1bf63479
jai kirdatt <jai.kirdatt@me.com>
2019-01-14 20:28:58 -0500

    navigate with numbers on play screen

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -3,6 +3,7 @@ window.onload = function() {
   let url = window.location.href;
   const sections = document.getElementsByTagName("section");
   let nextSectionToScrollIndex = 0;
+  let key = [];
 
   function keyPressed(e){
     if(e.keyCode == 39 || e.keyCode == 32){
@@ -19,23 +20,39 @@ window.onload = function() {
         return;
       nextSectionToScrollIndex--;
     }
+    else if (e.keyCode >= 48 && e.keyCode <= 57) {
+      key.push(e.key);
+      if(key.length == 2) {
+        nextSectionToScrollIndex = getSlideIndexToShowWithKey(key[0] + key[1]);
+      }
+    }
 
-    let $sectionToScroll = $(sections[nextSectionToScrollIndex]);
-    let $animation_elem = $sectionToScroll.next(".progress-bar");
-    $animation_elem.hide();
+    if (nextSectionToScrollIndex > 0) {
+      let $sectionToScroll = $(sections[nextSectionToScrollIndex]);
+      let $animation_elem = $sectionToScroll.next(".progress-bar");
+      $animation_elem.hide();
 
-    $("html, body").animate({
-      scrollTop: $sectionToScroll.offset().top
-    }, 2000, function(){
-      let $clone = $animation_elem.clone(true);
-      $animation_elem.before($clone);
-      $animation_elem.remove();
-      $clone.show();
-      setTimeout(function() {
-        $sectionToScroll.find("p").fadeOut();
-      }, fade_question_in_mseconds);
-    });
+      $("html, body").animate({
+        scrollTop: $sectionToScroll.offset().top
+      }, 2000, function(){
+        key = [];
+        nextSectionToScrollIndex = -1;
+        let $clone = $animation_elem.clone(true);
+        $animation_elem.before($clone);
+        $animation_elem.remove();
+        $clone.show();
+        setTimeout(function() {
+          $sectionToScroll.find("p").fadeOut();
+        }, fade_question_in_mseconds);
+      });
+    }
   }
 
   document.body.onkeyup = keyPressed;
+}
+
+function getSlideIndexToShowWithKey(key) {
+  let slideToStartWith = 5;
+  slideToStartWith += parseInt(key);
+  return slideToStartWith;
 }
