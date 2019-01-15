47c9698c8c04192289c3770bc336ef04b7a8706a
jai kirdatt <jai.kirdatt@me.com>
2019-01-14 19:32:42 -0500

    using venue email to send promotions

--- app/mailers/promotion_mailer.rb
+++ app/mailers/promotion_mailer.rb
@@ -3,14 +3,16 @@ class PromotionMailer < ApplicationMailer
     @team = params[:team]
     @subject = params[:subject]
     @promotional_text = params[:promotional_text]
-    @venue_name = @team.game.venue.name
+
     @team.players.each { |player|
       subscribed = Subscription.where(roster_id: player.roster_id)
                                .where("newsletter = true").count
 
       if subscribed = 1
         @player_name = player.roster.name
-        mail(to: player.roster_email, subject: @subject)
+        mail(from: @team.game.venue.email,
+               to: player.roster_email,
+          subject: @subject)
       end
     }
   end
